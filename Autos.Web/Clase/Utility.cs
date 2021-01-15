0f8294b93504c14e3ae8d79e5f6082871eb50efe
jasg0 <65267122+jasg0@users.noreply.github.com>
2021-01-14 19:49:51 -0800

    Cambios de Roles

--- Autos.Web/Clase/Utility.cs
+++ Autos.Web/Clase/Utility.cs
@@ -12,34 +12,39 @@ namespace Autos.Web.Clase
     {
         readonly static ApplicationDbContext db = new ApplicationDbContext();
 
-       
-
         public static void CheckRoles(string rol)
-            {
+        {
             var role = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
-            
+            if (!role.RoleExists(rol))
+            {
+                role.Create(new IdentityRole(rol));
             }
+        }
 
         internal static void CheckSuperUser()
         {
             var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
-
+            var user = userManager.FindByName("admin@autos.com");
+            if (user == null)
+            {
+                CreateSuperUser("admin@autos.com", "123456", null, "Administrador"); // Generar metodo click derecho
+            }
         }
 
-        private static void CreateSuperUser(string email, string password, string phone, string rol)
+        private static void CreateSuperUser(string email, string pass, string phone, string rol)
         {
             var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
             var user = new ApplicationUser()
             {
                 UserName = email,
-                Email = email,
+                Email = pass,
                 PhoneNumber = phone
-        };
-             
-             userManager.Create(user, password);
-             userManager.AddToRole(user.Id, rol);
+            };
 
+            userManager.Create(user, pass);
+            userManager.AddToRole(user.Id, rol);
         }
+
         public void Dispose()
         {
             db.Dispose();
